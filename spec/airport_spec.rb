889603c51ebdce1c555c245b2694d87306891166
Anjana <anjana.lpn@gmail.com>
2019-05-02 16:22:32 +0100

    User Story 2 As an air traffic controller So planes can take off safely from my airport I would like to instruct a plane to take off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,4 +8,10 @@ describe Airport do
       expect(airport).to respond_to(:land).with(1).argument
     end
   end
+
+  describe '#take_off' do
+    it 'instructs a plane to take off' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
+  end
 end
